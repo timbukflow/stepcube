1bd7f65af87757b313d89e563569bf408ecddd5a
Ivo Schwizer <ivoschwizer@macbook-pro-ssd.home>
2021-09-24 18:26:37 +0200

    blue home new

--- index.php
+++ index.php
@@ -14,28 +14,30 @@
     <section class="maincontainer">
       <article class="start">
         <h1 class="header">Wohnen und Arbeiten in der Hafenstadt Romanshorn</h1>
+        <img class="startimgone" src="img/stepcube-wohnen-romanshorn-loewenstrasse-vogelperspektive.jpg" alt="stepcube-wohnen-romanshorn-loewenstrasse-vogelperspektive" width="100%" height="auto">
 
-        <img class="startimg" src="img/stepcube-wohnen-in-romanshorn-start.jpg" alt="..." width="100%" height="auto">
-        <div class="color"></div>
+        <div class="holder">
+          <h2>Ihr sicherer Hafen am Bodensee</h2>
+          <p>An der Löwenstrasse in Romanshorn entsteht ein attraktiver und moderner Lebensraum für Menschen mit Liebe zum Bodensee. Die Überbauung «Stepcube» bietet 20 Mietwohnungen, 300 Quadratmeter Büro- und Gewerbefläche sowie 17 Parkplätze zu attraktiven Preisen in zentraler Lage zwischen Bahnhof, Hafen und See.</p>
+          <p class="second">Die moderne Architektur von Stepcube besteht aus zwei Würfeln, die miteinander verschmelzen – ein Ausdruck für das bunt durchmischte Lebensgefühl der Hafenstadt Romanshorn. Ob für Singles, Seefahrer, Selbstständige oder Senioren – die modern ausgestatteten Wohnungen mit ihren offenen Grundrissen erfüllen jegliche Ansprüche an ein aktives und mobiles Leben in Ufernähe.</p>
+          <a class="button" href="romanshorn">Entdecken Sie Romanshorn</a>
 
-        <h2>Ihr sicherer Hafen am Bodensee</h2>
-        <p>An der Löwenstrasse in Romanshorn entsteht ein attraktiver und moderner Lebensraum für Menschen mit Liebe zum Bodensee. Die Überbauung «Stepcube» bietet 20 Mietwohnungen, 300 Quadratmeter Büro- und Gewerbefläche sowie 17 Parkplätze zu attraktiven Preisen in zentraler Lage zwischen Bahnhof, Hafen und See. Ob für Singles, Seefahrer, Selbstständige oder Senioren – die modern ausgestatteten Wohnungen mit ihren offenen Grundrissen erfüllen jegliche Ansprüche an ein aktives und mobiles Leben in Ufernähe.
-        </p>
-
-        <div class="inputs">
-          <img src="img/stepcube-cross.svg" alt="stepcube-cross" width="30" height="30">
-          <h3>1463 m²<br>Wohnfläche</h3>
+          <div class="inputs">
+            <img src="img/stepcube-cross-w.svg" alt="stepcube-cross" width="30" height="30">
+            <h3>1463 m²<br>Wohnfläche</h3>
+          </div>
+          <div class="inputs iright">
+            <img src="img/stepcube-cross-w.svg" alt="stepcube-cross" width="30" height="30">
+            <h3>20 m zum<br>Seeufer</h3>
         </div>
       </article>
 
       <article class="wohnen" id="wohnen">
-        <div class="inputs">
-          <img src="img/stepcube-cross.svg" alt="stepcube-cross" width="30" height="30">
-          <h3>20 m zum<br>Seeufer</h3>
+
         </div>
-        <img class="wohnenimg nightimg" src="img/stepcube-wohnen-in-romanshorn-start-day.jpg" alt="..." width="100%" height="auto">
-        <img class="wohnenimg dayimg" src="img/stepcube-wohnen-in-romanshorn-start.jpg" alt="..." width="100%" height="auto">
-        <h1 class="header">Für Singles und Seefahrerinnen</h1>
+        <img class="wohnenimg nightimg" src="img/stepcube-wohnen-romanshorn-loewenstrasse-studio-1-zimmer-wohnung-nacht.jpg" alt="stepcube-wohnen-romanshorn-loewenstrasse-studio-1-zimmer-wohnung-nacht" width="100%" height="auto">
+        <img class="wohnenimg dayimg" src="img/stepcube-wohnen-romanshorn-loewenstrasse-studio-1-zimmer-wohnung-tag.jpg" alt="stepcube-wohnen-romanshorn-loewenstrasse-studio-1-zimmer-wohnung-tag" width="100%" height="auto">
+        <h1 class="header">Für Singles und Seefahrende</h1>
         <div class="daynightcontainer">
           <a class="day daybutton">Tag</a>
           <a class="night">Nacht</a>
@@ -45,14 +47,14 @@
         </p>
         <a class="button" href="wohnangebot">Grundrisse</a>
         <div class="wcontainer">
-          <img class="wohnenimgtwo" src="img/stepcube-wohnen-in-romanshorn-start.jpg" alt="..." width="100%" height="auto">
-          <img class="wohnenimgthree" src="img/stepcube-wohnen-in-romanshorn-start.jpg" alt="..." width="100%" height="auto">
+          <img class="wohnenimgtwo" src="img/stepcube-wohnen-romanshorn-loewenstrasse-zimmer-wohnung-kueche.jpg" alt="stepcube-wohnen-romanshorn-loewenstrasse-zimmer-wohnung-kueche" width="100%" height="auto">
+          <img class="wohnenimgthree" src="img/stepcube-wohnen-romanshorn-loewenstrasse-zimmer-wohnung-schlafzimmer.jpg" alt="stepcube-wohnen-romanshorn-loewenstrasse-zimmer-wohnung-schlafzimmer" width="100%" height="auto">
           <div class="inputs ">
-            <img src="img/stepcube-cross.svg" alt="stepcube-cross" width="30" height="30">
+            <img src="img/stepcube-cross-w.svg" alt="stepcube-cross" width="30" height="30">
             <h3>25 min<br>nach St.Gallen</h3>
           </div>
         </div>
-        <h1 class="header two">Für Sonnenanbeter und frisch Verliebte</h1>
+        <h1 class="header two">Für Sonnenanbetende und frisch Verliebte</h1>
         <h2>2.5- und 3.5-Zimmer-Wohnungen</h2>
         <p>Darf es noch ein bisschen mehr sein? Geniessen Sie die Morgensonne mit Kaffee und Zeitung in der Loggia direkt neben dem lichtdurchfluteten Schlafzimmer. Das grosszügige Wohnzimmer mit offener Küche lädt zum Verweilen ein. Empfangen Sie Ihre Liebsten Abends zum Grillen auf der Dachterrasse oder entspannen Sie an einem Winterabend gemütlich in der Badewanne. Ein eigener Waschturm spart Zeit für die optimale Work-Life-Balance. Während der Trockner läuft, noch auf eine Runde mit dem E-Bike? Kein Problem, es steht geladen im eigenen Kellerabteil bereit.
         </p>
@@ -60,10 +62,10 @@
       </article>
 
       <article class="gewerbe" id="gewerbe">
-        <img class="gewerbeimg " src="img/stepcube-wohnen-in-romanshorn-start.jpg" alt="..." width="100%" height="auto">
+        <img class="gewerbeimg " src="img/stepcube-wohnen-romanshorn-loewenstrasse-gewerbe-bueros.jpg" alt="stepcube-wohnen-romanshorn-loewenstrasse-gewerbe-bueros" width="100%" height="auto">
         <h1 class="header">Für Startups und Selbstständige</h1>
         <div class="inputs">
-          <img src="img/stepcube-cross.svg" alt="stepcube-cross" width="30" height="30">
+          <img src="img/stepcube-cross-w.svg" alt="stepcube-cross" width="30" height="30">
           <h3>205 m²<br>Gewerbe</h3>
         </div>
         <h2>Gewerbe und Büros</h2>
