8ae35ad574e02e81d3a4752aee47fb77beab4a04
Ivo Schwizer <ivoschwizer@gmail.com>
2022-03-10 08:21:56 +0100

    up

--- wohnen-gewerbe-buero-angebot.php
+++ wohnen-gewerbe-buero-angebot.php
@@ -393,10 +393,10 @@
                   <td>3.5</td>
                   <td>74.0</td>
                   <td>7.0</td>
-                  <td colspan="3">reserviert</td>
-                  <!--<td>1'470</td>
+                  <!--<td colspan="3">reserviert</td>-->
+                  <td>1'470</td>
                   <td>220</td>
-                  <td>1'690</td>-->
+                  <td>1'690</td>
                   <td class="planb">
                       <img class="plani" src="img/plan.svg" alt="plan icon" width="21" height="18">
                       <div class="plan">
