d1075453ed34cc30d4f301a44f8fb57783f1eb13
Ivo Schwizer <ivoschwizer@gmail.com>
2022-08-18 16:42:51 +0200

    update

--- wohnen-gewerbe-buero-angebot.php
+++ wohnen-gewerbe-buero-angebot.php
@@ -610,10 +610,10 @@
                   <td>1.5</td>
                   <td>30.0</td>
                   <td>0.0</td>
-                  <!--<td colspan="3">vermietet</td>-->
-                  <td>820</td>
+                  <td colspan="3">reserviert</td>
+                  <!--<td>820</td>
                   <td>100</td>
-                  <td>920</td>
+                  <td>920</td>-->
                   <td class="planb">
                       <img class="plani" src="img/plan.svg" alt="plan icon" width="21" height="18">
                       <div class="plan">
@@ -631,10 +631,10 @@
                   <td>1.5</td>
                   <td>30.0</td>
                   <td>0.0</td>
-                  <!--<td colspan="3">reserviert</td>-->
-                  <td>820</td>
+                  <td colspan="3">reserviert</td>
+                  <!--<td>820</td>
                   <td>100</td>
-                  <td>920</td>
+                  <td>920</td>-->
                   <td class="planb">
                       <img class="plani" src="img/plan.svg" alt="plan icon" width="21" height="18">
                       <div class="plan">
@@ -897,10 +897,10 @@
                   <td>1.5</td>
                   <td>29.0</td>
                   <td>0.0</td>
-                  <!--<td colspan="3">reserviert</td>-->
-                  <td>810</td>
+                  <td colspan="3">vermietet</td>
+                  <!--<td>810</td>
                   <td>100</td>
-                  <td>910</td>
+                  <td>910</td>-->
                   <td class="planb">
                       <img class="plani" src="img/plan.svg" alt="plan icon" width="21" height="18">
                       <div class="plan">
@@ -1275,10 +1275,10 @@
                   <td>Büro</td>
                   <td>59.0</td>
                   <td>0.0</td>
-                  <!--<td colspan="3">reserviert</td>-->
-                  <td>1'190</td>
+                  <td colspan="3">reserviert</td>
+                  <!--<td>1'190</td>
                   <td>180</td>
-                  <td>1'370</td>
+                  <td>1'370</td>-->
                   <td class="planb">
                       <img class="plani" src="img/plan.svg" alt="plan icon" width="21" height="18">
                       <div class="plan">
